169544d67f2e916e510e9f19a8332d31ffbab4be
subhambiswajit <digu35@gmail.com>
2016-04-03 01:00:33 +0530

    django project ready

---
+++ apps/backends/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from django.conf import settings
+
+
+
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'slamit.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+     # url(r'^signup/$', views.signup, name="signup"),
+     # url(r'signin/$', views.login_user, name="auth_login"),
+     # url(r'^$', TemplateView.as_view(template_name='login/login.html')),
+    
+)
