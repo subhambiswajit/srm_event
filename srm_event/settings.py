c7123bbe52ffb6925af15fdb160fe34187c9220a
subhambiswajit <digu35@gmail.com>
2016-04-03 01:27:05 +0530

    models.pyupdate

--- srm_event/settings.py
+++ srm_event/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.backends',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -53,6 +54,17 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+    'django.core.context_processors.csrf',
+    'django.contrib.messages.context_processors.messages'
 )
 
 ROOT_URLCONF = 'srm_event.urls'
