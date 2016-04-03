ebdde90bcd13a331752a1ccd597879f63e1b55c2
subhambiswajit <digu35@gmail.com>
2016-04-03 20:22:23 +0530

    lets begin

--- srm_event/settings.py
+++ srm_event/settings.py
@@ -80,13 +80,13 @@ DATABASES = {
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.mysql',
         # Or path to database file if using sqlite3.
-        'NAME': 'u551441840_srm',
+        'NAME': 'srm_master',
         # The following settings are not used with sqlite3:
-        'USER': 'u551441840_srm',
-        'PASSWORD': 'srmproject',
+        'USER': 'root',
+        'PASSWORD': '',
         # Empty for localhost through domain sockets or '127.0.0.1' for localhost
         # through TCP.
-        'HOST': 'mysql.hostinger.in',
+        'HOST': 'localhost',
         'PORT': '',                      # Set to empty string for default.
     }
 }
