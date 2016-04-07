0965df0f798aadb916d670d12f181c8b9557f786
subhambiswajit <digu35@gmail.com>
2016-04-08 00:08:14 +0530

    anusha joined

--- apps/backends/views.py
+++ apps/backends/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from apps.backends.models import *
+import datetime
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-
+@csrf_exempt
 def personal_details(request):
-
-
-
-
-
-
-	return HttpResponse("the details are saved")
+	data = ''
+	print request.GET['candidate_name']
+	print request.GET['candidate_regno']
+	if request.method == 'GET':
+		personal_detail = CandidateMaster()
+		personal_detail.candidate_name = request.GET['candidate_name']
+		if 'candidate_type' in request.GET:
+			personal_detail.candidate_type = request.GET['candidate_type']
+		personal_detail.candidate_year = datetime.datetime.now()
+		personal_detail.candidate_date = datetime.datetime.now()
+		if 'candidate_regno' in request.GET:
+			personal_detail.candidate_reg_number = request.GET['candidate_regno']
+		personal_detail.save()
+	data = "success"
+	return HttpResponse(data)
--- srm_event/urls.py
+++ srm_event/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,3 +10,13 @@ urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^',include('apps.backends.urls'))
 )
+if settings.DEBUG:
+    from django.views.static import serve
+    _media_url = settings.MEDIA_URL
+    if _media_url.startswith('/'):
+        _media_url = _media_url[1:]
+        urlpatterns += patterns('',
+                                (r'^%s(?P<path>.*)$' % _media_url,
+                                serve,
+                                {'document_root': settings.MEDIA_ROOT}))
+    del(_media_url, serve)
