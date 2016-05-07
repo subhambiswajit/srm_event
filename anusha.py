3d5aecd7f72c201182f61b93093c3638ac522f28
subhambiswajit <digu35@gmail.com>
2016-05-08 00:28:02 +0530

    image field added to database

--- anusha.py
+++ anusha.py
@@ -1,37 +1,84 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [app_label]'
+# into your database.
+from __future__ import unicode_literals
+
 from django.db import models
-from imagekit.processors import ResizeToFill
-from imagekit.models import ImageSpecField
-# Create your models here.
 
 
-class GlobalUsers(models.Model):
-    gus_userid = models.IntegerField(primary_key=True)
-    gus_picture = models.FileField(blank=True, null=True, upload_to="uploads")
-    image_thumbnail = ImageSpecField(source='gus_picture',
-                                     processors=[ResizeToFill(233, 233)],
-                                     format='JPEG',
-                                     options={'quality': 60})
-    gus_username = models.CharField(max_length=100, blank=True)
-    gus_name = models.CharField(max_length=100, blank=True)
-    gus_password = models.CharField(max_length=100, blank=True)
-    last_login = models.DateTimeField(db_column='gus_lastlogin', blank=True, null=True)
-    gus_type = models.CharField(max_length=30, blank=True)
-    gus_createdby = models.CharField(max_length=40, blank=True, null=True)
-    gus_dob = models.DateTimeField(blank=True, null=True)
-    gus_email = models.CharField(max_length=50, blank=True, null=True)
-    gus_isused = models.IntegerField(blank=True, null=True)
-    gus_modifiedon = models.DateTimeField(blank=True, null=True)
-    gus_verify = models.CharField(max_length=10, blank=True)
+class AuthGroup(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    name = models.CharField(unique=True, max_length=80)
 
     class Meta:
         managed = False
-        db_table = 'global_users'
-    def is_authenticated(user):
-        return True
-    def is_active(self):
-        return True
-    def is_staff(self):
-        return True
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    group = models.ForeignKey(AuthGroup)
+    permission = models.ForeignKey('AuthPermission')
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+
+
+class AuthPermission(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    name = models.CharField(max_length=50)
+    content_type = models.ForeignKey('DjangoContentType')
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+
+
+class AuthUser(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField()
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=75)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    user = models.ForeignKey(AuthUser)
+    group = models.ForeignKey(AuthGroup)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    user = models.ForeignKey(AuthUser)
+    permission = models.ForeignKey(AuthPermission)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+
 
 class CandidateActivity(models.Model):
     candidate_id = models.IntegerField(primary_key=True)
@@ -41,32 +88,22 @@ class CandidateActivity(models.Model):
     cand_act_prize = models.TextField(blank=True)
     cand_act_nature = models.TextField(blank=True)
     cand_act_year = models.CharField(max_length=100, blank=True)
+
     class Meta:
         managed = False
         db_table = 'candidate_activity'
 
-class CandidatePerformance(models.Model):
-    cand_per_id = models.IntegerField(primary_key=True)
-    cand_per_gusid = models.ForeignKey('GlobalUsers', db_column='cand_per_gusid', blank=True, null=True)
-    cand_per_exam = models.CharField(max_length=100, blank=True)
-    cand_per_ypass = models.CharField(max_length=30, blank=True)
-    cand_per_marks = models.CharField(max_length=30, blank=True)
-    cand_per_year = models.CharField(max_length=30, blank=True)
+
+class CandidateDevelopment(models.Model):
+    cand_dev_id = models.IntegerField(primary_key=True)
+    cand_dev_gusid = models.ForeignKey('GlobalUsers', db_column='cand_dev_gusid', blank=True, null=True)
+    cand_dev_name = models.TextField()
+    cand_dev_faculty = models.TextField()
 
     class Meta:
         managed = False
-        db_table = 'candidate_performance'
+        db_table = 'candidate_development'
 
-class CandidateNationalRecognition(models.Model):
-    cand_nat_reg_id = models.IntegerField(primary_key=True)
-    cand_nat_reg_gus = models.ForeignKey('GlobalUsers', db_column='cand_nat_reg_gus_id', blank=True, null=True)
-    cand_nat_reg_details = models.TextField(blank=True)
-    cand_nat_reg_year = models.CharField(max_length=30, blank=True)
-    cand_nat_reg_isused = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'candidate_national_recognition'
 
 class CandidateInitiatives(models.Model):
     cand_ini_id = models.IntegerField(primary_key=True)
@@ -81,13 +118,14 @@ class CandidateInitiatives(models.Model):
         managed = False
         db_table = 'candidate_initiatives'
 
+
 class CandidateInternship(models.Model):
     cand_int_id = models.IntegerField(primary_key=True)
     cand_int_gusid = models.ForeignKey('GlobalUsers', db_column='cand_int_gusid', blank=True, null=True)
     cand_int_name = models.CharField(max_length=100, blank=True)
-    cand_int_start = models.DateTimeField()
-    cand_int_end = models.DateTimeField()
-    cand_int_stipend = models.CharField(max_length=100)
+    cand_int_start = models.DateTimeField(blank=True, null=True)
+    cand_int_end = models.DateTimeField(blank=True, null=True)
+    cand_int_stipend = models.CharField(max_length=100, blank=True)
 
     class Meta:
         managed = False
@@ -111,6 +149,19 @@ class CandidateJournals(models.Model):
         managed = False
         db_table = 'candidate_journals'
 
+
+class CandidateNationalRecognition(models.Model):
+    cand_nat_reg_id = models.IntegerField(primary_key=True)
+    cand_nat_reg_gus = models.ForeignKey('GlobalUsers', blank=True, null=True)
+    cand_nat_reg_details = models.TextField(blank=True)
+    cand_nat_reg_year = models.CharField(max_length=30, blank=True)
+    cand_nat_reg_isused = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'candidate_national_recognition'
+
+
 class CandidatePaperConference(models.Model):
     cand_pap_conf_id = models.IntegerField(primary_key=True)
     cand_pap_conf_gusid = models.ForeignKey('GlobalUsers', db_column='cand_pap_conf_gusid', blank=True, null=True)
@@ -127,15 +178,66 @@ class CandidatePaperConference(models.Model):
         managed = False
         db_table = 'candidate_paper_conference'
 
-class CandidateDevelopment(models.Model):
-    cand_dev_id = models.IntegerField(primary_key=True)
-    cand_dev_gusid = models.ForeignKey('GlobalUsers', db_column='cand_dev_gusid', blank=True, null=True)
-    cand_dev_name = models.TextField()
-    cand_dev_faculty = models.TextField()
+
+class CandidatePerformance(models.Model):
+    cand_per_id = models.IntegerField(primary_key=True)
+    cand_per_gusid = models.ForeignKey('GlobalUsers', db_column='cand_per_gusid', blank=True, null=True)
+    cand_per_exam = models.CharField(max_length=100, blank=True)
+    cand_per_ypass = models.CharField(max_length=30, blank=True)
+    cand_per_marks = models.CharField(max_length=30, blank=True)
+    cand_per_year = models.CharField(max_length=30, blank=True)
 
     class Meta:
         managed = False
-        db_table = 'candidate_development'
+        db_table = 'candidate_performance'
+
+
+class DjangoAdminLog(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.IntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', blank=True, null=True)
+    user = models.ForeignKey(AuthUser)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    name = models.CharField(max_length=100)
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+
+
+class DjangoMigrations(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
 
 class FacAwards(models.Model):
     fac_awards_id = models.IntegerField(primary_key=True)
@@ -269,3 +371,21 @@ class FacSoftwareDevelopment(models.Model):
         db_table = 'fac_software_development'
 
 
+class GlobalUsers(models.Model):
+    gus_userid = models.IntegerField(primary_key=True)
+    gus_picture = models.CharField(max_length=200, blank=True)
+    gus_username = models.CharField(max_length=100, blank=True)
+    gus_name = models.CharField(max_length=100, blank=True)
+    gus_password = models.CharField(max_length=100, blank=True)
+    gus_lastlogin = models.DateTimeField(blank=True, null=True)
+    gus_type = models.CharField(max_length=30, blank=True)
+    gus_createdby = models.CharField(max_length=40, blank=True)
+    gus_dob = models.DateTimeField(blank=True, null=True)
+    gus_email = models.CharField(max_length=50, blank=True)
+    gus_isused = models.IntegerField(blank=True, null=True)
+    gus_modifiedon = models.DateTimeField(blank=True, null=True)
+    gus_verify = models.CharField(max_length=10, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'global_users'
